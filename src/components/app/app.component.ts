33653a22da23bd0cdf44941843928f793e986b86
Erlend Bjerke Gabrielsen <erlend.gabrielsen@soprasteria.com>
2016-12-11 12:15:54 +0100

    added framework components

---
+++ src/components/app/app.component.ts
@@ -0,0 +1,7 @@
+    import { Component } from '@angular/core';
+
+@Component({
+  selector: 'statnett',
+  template: `<h1>Hello {{name}}</h1>`,
+})
+export class AppComponent  { name = 'Statnett'; }
